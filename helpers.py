44c247f53dc7b132d114fe8fd72b85a287c1c4e0
ealex_mondra <ealejandro.mondragont@gmail.com>
2019-01-28 13:24:13 -0600

    final

--- helpers.py
+++ helpers.py
@@ -3,26 +3,27 @@
 import os
 import glob # library for loading images from a directory
 import cv2
+from enum import Enum
 
 # This function loads in images and their labels and places them in a list
 # The list contains all images and their associated labels
 # For example, after data is loaded, im_list[0][:] will be the first image-label pair in the list
 def load_dataset(image_dir):
-    
+
     # Populate this empty image list
     im_list = []
     image_types = ["day", "night"]
-    
+
     # Iterate through each color folder
     for im_type in image_types:
-        
+
         # Iterate through each image file in each image_type folder
         # glob reads in any image with the extension "image_dir/im_type/*"
         for file in glob.glob(os.path.join(image_dir, im_type, "*")):
-            
+
             # Read in the image
             im = cv2.imread(file)
-            
+
             # Check if the image exists/if it's been correctly read-in
             if not im is None:
                 # Append the image, and it's type (red, green, yellow) to the image list
@@ -35,39 +36,37 @@ def load_dataset(image_dir):
 # Resize each image to the desired input size: 600x1100px (hxw).
 # This function should take in an RGB image and return a new, standardized version
 def standardize_input(image, width, height):
-    
+
     # Resize image and pre-process so that all "standard" images are the same size
     # cv2.resize
-    standard_im = none
-    
+    standard_im = cv2.resize(image,(width, height), interpolation = cv2.INTER_CUBIC)
+
     return standard_im
 
-# With each loaded image, we also specify the expected output. 
+# With each loaded image, we also specify the expected output.
 # For this, we use binary numerical values 0/1 = night/day.
-# Examples: 
+# Examples:
 # encode("day") should return: 1
 # encode("night") should return: 0
+
 def encode(label):
-        
-    numerical_val = 0
-    
-    return numerical_val
+    return 1 if label=="day" else 0
+def decode(label):
+    return "night" if label == 0 or label == False else "day"
 
 
 ## Standardize the output using both functions above, standardize the input images and output labels
 def standardize(image_list):
-    
     # Empty image data array
     standard_list = []
 
     # Iterate through all the image-label pairs
     for item in image_list:
-        
         # Standardize the image
-        
+        standar_image = standardize_input(item[0],1100, 600 )
         # Create a numerical label
-        
-        # Append the image, and it's one hot encoded label to the full, processed list of image data 
-        
-        
+        item_label = encode(item[1])
+        # Append the image, and it's one hot encoded label to the full, processed list of image data
+        standard_list.append((standar_image,item_label))
+
     return standard_list
